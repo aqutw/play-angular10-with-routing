ccaba1e32e9269bfe65288c109e3f8ac5d86f021
aqutw <aquarian.EX@hotmail.com>
2020-08-09 22:57:11 +0800

    Completed Basic Routing at #24 https://www.youtube.com/watch?v=yymtGNGyhuM&list=PL8p2I9GklV45JZerGMvw5JVxPSxCg8VPv&index=24

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
+import { UserComponent } from './user/user.component';
+import { AdminComponent } from './admin/admin.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'user',
+    component: UserComponent
+  },
+  {
+    path: 'admin',
+    component: AdminComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
